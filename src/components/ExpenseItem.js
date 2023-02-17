8cdd9fff445185dfd457f81ba8b930bcccea4ba3
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-02-17 20:39:10 -0300

    Initial commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 1, 14);
-  console.log(expenseDate);
+function ExpenseItem({ title, date, amount }) {
   return (
     <div className="expense-item">
-      <div>asdasd</div>
+      <div>{date.toLocaleString("es-AR")}</div>
       <div className="expense-item__description">
-        <h2> Car Insurance </h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2> {title} </h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
